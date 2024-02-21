07e9ca32a0c922f62cd4868f68fecebd7c9c7b12
suraj pro <71000909+suraj719@users.noreply.github.com>
2024-02-21 21:31:14 +0530

    first commit

---
+++ src/components/shared/Navbar.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Oval } from "react-loader-spinner";
+
+export default function Navbar() {
+  const navigate = useNavigate();
+  const [loading, setIsLoading] = useState(false);
+
+  return (
+    <>
+      <div className="py-5 flex items-center justify-between p-3 bg-gray-800">
+        <Link to="/">
+          <div className="flex items-center text-white">
+            {/* <img src="/innerserenity.png" style={{ width: "12rem" }} /> */}
+            <p>EduWave</p>
+          </div>
+        </Link>
+
+        <div className="mx-0 lg:mx-5">
+          {loading ? (
+            <>
+              <div className=" text-black font-bold rounded-full w-40">
+                <div className="flex justify-center items-center">
+                  <Oval
+                    height={40}
+                    width={40}
+                    color="#4fa94d"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                    ariaLabel="oval-loading"
+                    secondaryColor="#4fa94d"
+                    strokeWidth={2}
+                    strokeWidthSecondary={2}
+                  />
+                </div>
+              </div>
+            </>
+          ) : (
+            <div className="text-white">
+              <p>Login</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
