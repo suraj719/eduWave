1700b1cac2b3a7d020f97975bbbda48c9ed36937
suraj pro <71000909+suraj719@users.noreply.github.com>
2024-02-26 18:28:37 +0530

    fix: env

--- server/models/TeacherModel.js
+++ server/models/TeacherModel.js
@@ -4,6 +4,10 @@ const TeacherSchema = new mongoose.Schema({
   name: String,
   email: String,
   password: String,
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("teachers", TeacherSchema);
